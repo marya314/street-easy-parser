c120741d8b5d3856d2e12608ef82d2a4d606b1d5
marya314 <mary.boyd314@gmail.com>
2017-03-02 20:04:57 -0500

    added json_file_open method

--- lib/runner.rb
+++ lib/runner.rb
@@ -13,34 +13,12 @@ class Runner
     def scrape_data
         #get all sales and rental listings
         response = @top_listings.get_listings
-        # binding.pry
         #get parsed data array of hashes
         @parsed_data = Parser.new
-        # binding.pry
         parsed_response = @parsed_data.get_parsed_listings(response)
         json_data = response_to_json(parsed_response)
-        binding.pry
-        # json_file_open(json_data)
-        binding.pry
+        json_file_open(json_data)
     end
-
-    #get sales and rental listing data
-  #   def get_listing_data
-  #       @all_listings = @top_listings.get_listings
-  #       # @all_listings
-  #       # @all_sales_listings = @top_listings.all_sales_listings
-  #       # @all_rental_listings = @top_listings.all_rental_listings
-  #       # @all_sales_listings
-  #       # @all_rental_listings
-  # binding.pry
-  #   end
-
-    #get parsed data as a two arrays of hashes containing required info
-    # def get_parsed_data(response)
-    #      binding.pry
-    #     # @parsed_data = Parser.new(all_sales_listings, all_rental_listings)
-    #     @parsed_data.get_parsed_listings(response)
-    # end
 
 private
     #convert data to json and push into an array
@@ -52,12 +30,12 @@ private
             end
         end
 
-        # put array of json data into file and open it
-        # def json_file_open(json_data)
-        #     File.open("listings.json", "w+") do |f|
-        #       f.write(JSON.pretty_generate(listings.flatten))
-        #     end
-        # end
+        #put array of json data into file and open it
+        def json_file_open(json_data)
+            File.open("listings.json", "w") do |f|
+              f.write(JSON.pretty_generate(json_data.flatten))
+            end
+        end
 end
 
 #run program
